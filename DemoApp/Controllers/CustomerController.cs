33301b9d0460c43cdbcc71bbe3715368b39d0ede
KasperMoss <kaspermoss@gmail.com>
2022-01-17 21:25:33 +0100

    Added customer controller

---
+++ DemoApp/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using DemoApp.Models;
+using DemoApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoApp.Controllers
+{
+    [Route("api/customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public Customer Get(int id)
+        {
+            return _customerService.Get(id);
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _customerService.Create(value);
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
