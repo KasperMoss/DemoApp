71b7ea069852913237267d894e8612bf14e5ea33
KasperMoss <kaspermoss@gmail.com>
2022-01-17 21:25:19 +0100

    Added customer service with interface

---
+++ DemoApp/Services/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using DemoApp.Models;
+using DemoApp.Repositories;
+
+namespace DemoApp.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        public Customer Create(string name)
+        {
+            return _customerRepository.Create(name);
+        }
+
+        public Customer Get(int Id)
+        {
+            return _customerRepository.Get(Id);
+        }
+    }
+}
---
+++ DemoApp/Services/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using DemoApp.Models;
+
+namespace DemoApp.Services
+{
+    public interface ICustomerService
+    {
+        Customer Get(int Id);
+
+        Customer Create(string name);
+    }
+}
